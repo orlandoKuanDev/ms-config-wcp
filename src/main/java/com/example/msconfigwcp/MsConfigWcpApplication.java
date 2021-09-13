778e762bf19ca9c4b3772da7b30a56ed01ac6d4d
Orlando Kuan <orlandojose.kuanbecerra@everis.nttdata.com>
2021-09-13 17:10:55 -0500

    Initial commit

---
+++ src/main/java/com/example/msconfigwcp/MsConfigWcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.msconfigwcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConfigWcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsConfigWcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msconfigwcp/MsConfigWcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msconfigwcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigWcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
